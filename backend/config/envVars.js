87cff1db7de9918a62d15091a56ab9ed592076c4
Amanpati Tripathi <tripathiaman777@gmail.com>
2024-08-25 14:31:50 +0530

    First Commit

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV_VARS = {
-	MONGO_URI: process.env.MONGO_URI,
+	MONGO_URI: process.env.MONGODB_URI,
 	PORT: process.env.PORT || 5000,
 	JWT_SECRET: process.env.JWT_SECRET,
 	NODE_ENV: process.env.NODE_ENV,
